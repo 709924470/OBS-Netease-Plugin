2b3f27836d0e407c6de6089e0aae1e58078e2d3e
Akatsuki.Kinokuni <rec_000@126.com>
2021-06-11 05:43:46 +0800

    Major changes: Use argparse for arguments Add: Bilibili subscriber counter w/ formats

--- file_watcher.py
+++ file_watcher.py
@@ -1,5 +1,7 @@
 import sys, os
 import json
+import argparse as argp
+import urllib.request as ul2
 from threading import Thread
 from time import sleep
 
@@ -16,31 +18,45 @@ from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
 # monkey.patch_all()
 
+arguments = argp.ArgumentParser()
+mutex = arguments.add_mutually_exclusive_group(required=True)
+mutex.add_argument("-m", "--mode", type=str, help="Choose from UWP or DESKTOP version of Netease Cloud music")
+mutex.add_argument("-H", "--history", type=str, help="Play history file path")
+# Bilibili subcount arguments
+group = arguments.add_argument_group()
+group.add_argument("--vmid", type=str, help="Track bilibili account subscriber count(uses uid, not live channel id)")
+group.add_argument("-o", "--out-dir", type=str, help="Text output for obs to load", default="./out.txt")
+group.add_argument("-f", "--format", type=str, help="Format for output, use $c for current subscriber count", default="Subscriber count: $c\nGoal: 1000")
+
 # C:\Users\[User]\AppData\Local\Packages\[PACKAGE ID]\LocalCache\Local\Netease\CloudMusic\webdata\file\history
 # %appdata%/../local/netease/cloudmusic/webdata/file/
+args = arguments.parse_args()
 terminated = False
-history_dir = None if len(sys.argv) == 1 else sys.argv[1]
-if 'UWP' in history_dir.upper():
+history_dir = None
+if args.mode and 'UWP' in args.mode:
     for p, d, f in os.walk(os.path.join(os.getenv("appdata"),"../Local/Packages/")):
         if "CloudMusic" in d:
-            # print( os.path.join(p,d[0]))
             history_dir = os.path.join(p, d[0]) +  "\\webdata\\file\\"
             break
-if 'DESKTOP' in history_dir.upper():
+if args.mode and 'DESKTOP' in args.mode:
     if os.path.exists(os.path.join(os.getenv("appdata"),"../local/netease/cloudmusic/webdata/file/")):
             history_dir = os.path.join(os.getenv("appdata"),"../local/netease/cloudmusic/webdata/file/")
+if not args.mode:
+    if args.history:
+        history_dir = args.history
 if not history_dir:
     for p, d, f in os.walk(os.path.join(os.getenv("appdata"),"../Local/Packages/")):
         if "CloudMusic" in d:
-            # print( os.path.join(p,d[0]))
+            print("Found UWP version history file")
             history_dir = os.path.join(p, d[0]) +  "\\webdata\\file\\"
             break
     if not history_dir:
         if os.path.exists(os.path.join(os.getenv("appdata"),"../local/netease/cloudmusic/webdata/file/")):
+            print("Found DESKTOP version history file")
             history_dir = os.path.join(os.getenv("appdata"),"../local/netease/cloudmusic/webdata/file/")
 
 if not history_dir or history_dir.upper() in ['UWP', 'DESKTOP']:
-    print("Cannot find UWP Netease Cloudmusic history json directory, please specify by using", sys.argv[0], "[history json directory]")
+    print("Cannot find UWP Netease Cloudmusic history json directory, please specify by using", sys.argv[0], "[-H history json directory]")
     os.system("pause")
     exit(-1)
 
@@ -133,12 +149,34 @@ def conn(ws):
             ws.send("PING")
     print("user disconnected")
 
+def timedUpdate():
+    global args
+    format_str = args.format.replace("\\n", '\n') if args.format else None
+    while True and args.vmid and args.format:
+        try:
+            resp = ul2.urlopen("https://api.bilibili.com/x/relation/stat?vmid=" + args.vmid)
+            stat = resp.read().decode("utf-8", "ignore")
+            stat = json.loads(stat)
+            resp.close()
+            stat = stat["data"]["follower"]
+            if os.path.exists(args.out_dir):
+                os.remove(args.out_dir)
+            with open(args.out_dir, "w", encoding="utf8") as out:
+                out.write(format_str.replace("$c", str(stat)))
+        except Exception as e:
+            print(e)
+        finally:
+            sleep(20)
+    pass
+
 if __name__ == "__main__":
     on_file_change()
     monitor = Thread(target=fileMonitor)
     monitor.setDaemon(True)
     monitor.start()
+    subcounter = Thread(target=timedUpdate)
+    subcounter.setDaemon(True)
+    subcounter.start()
     server = pywsgi.WSGIServer(('0.0.0.0', 9999), app, handler_class=WebSocketHandler)
     server.serve_forever()
     # app.run(host="0.0.0.0", debug=False, port="9999")
-
